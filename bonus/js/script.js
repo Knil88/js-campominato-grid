da69de397600639fbfcde9a11e7da9646d526bdd
Yari <narducciyarigmail.com>
2022-11-16 17:37:14 +0100

    impostazione condizioni

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,18 +1,18 @@
 //Campo Minato
+    
+
+let myBtn = document.querySelector("#play");
+
+myBtn.addEventListener("click",
+function (){
     //Creiamo la variabile container dove appenderemo i nostri elementi
     let container = document.getElementById("container-grid");
 
     console.log(container);
     
     let difficult = document.getElementById("difficoltà");
-
-let myBtn = document.querySelector("#play");
-
-myBtn.addEventListener("click",
-function (){
-
-
-    container.innerHTML= "";
+if(difficult == "normal"){
+    document.getElementById("container-grid").innerHTML="";
     for(let i=1 ; i<=100; i++){
         
         let nuovoElemento = createBox("div","box");
@@ -32,6 +32,53 @@ function (){
     
         
     }
+}
+ if(difficult == "hard"){
+   document.getElementById("container-grid").innerHTML="";
+    for(let i=1 ; i<=81; i++){
+        
+        let nuovoElemento = createBox("div","box");
+        
+        container.appendChild(nuovoElemento);
+
+        nuovoElemento.append(i);
+         nuovoElemento.addEventListener("click",
+         function(){
+            nuovoElemento.classList.toggle("clicked");
+            console.log("Stai cliccando il numero",i);
+         }
+         )
+       
+        
+        
+    
+        
+    }
+     if(difficult == "very-hard"){
+        document.getElementById("container-grid").innerHTML="";
+        for(let i=1 ; i<=49; i++){
+            
+            let nuovoElemento = createBox("div","box");
+            
+            container.appendChild(nuovoElemento);
+    
+            nuovoElemento.append(i);
+             nuovoElemento.addEventListener("click",
+             function(){
+                nuovoElemento.classList.toggle("clicked");
+                console.log("Stai cliccando il numero",i);
+             }
+             )
+           
+            
+            
+        
+            
+        }
+    }
+}
+
+    
 } 
 
 )
