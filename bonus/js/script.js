81bf38d784b2a7aa7cf451c2fd702e80cbca1cbc
Yari <narducciyarigmail.com>
2022-11-16 18:16:25 +0100

    Completamento delle difficoltà

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,62 +1,18 @@
 //Campo Minato
-    
+    //Creiamo la variabile container dove appenderemo i nostri elementi
+let container = document.getElementById("container-grid");
+
+console.log(container);
+
+let difficult = document.getElementById("difficoltà");
 
 let myBtn = document.querySelector("#play");
 
 myBtn.addEventListener("click",
 function (){
-    //Creiamo la variabile container dove appenderemo i nostri elementi
-    let container = document.getElementById("container-grid");
-
-    console.log(container);
-    
-    let difficult = document.getElementById("difficoltà");
-if(difficult == "normal"){
-    document.getElementById("container-grid").innerHTML="";
-    for(let i=1 ; i<=100; i++){
-        
-        let nuovoElemento = createBox("div","box");
-        
-        container.appendChild(nuovoElemento);
-
-        nuovoElemento.append(i);
-         nuovoElemento.addEventListener("click",
-         function(){
-            nuovoElemento.classList.add("clicked");
-            console.log("Stai cliccando il numero",i);
-         }
-         )
-       
-        
-        
-    
-        
-    }
-}
- if(difficult == "hard"){
-   document.getElementById("container-grid").innerHTML="";
-    for(let i=1 ; i<=81; i++){
-        
-        let nuovoElemento = createBox("div","box");
-        
-        container.appendChild(nuovoElemento);
-
-        nuovoElemento.append(i);
-         nuovoElemento.addEventListener("click",
-         function(){
-            nuovoElemento.classList.add("clicked");
-            console.log("Stai cliccando il numero",i);
-         }
-         )
-       
-        
-        
-    
-        
-    }
-     if(difficult == "very-hard"){
-        document.getElementById("container-grid").innerHTML="";
-        for(let i=1 ; i<=49; i++){
+    if(difficult.value == "normal"){
+        container.innerHTML= "";
+        for(let i=1 ; i<=100; i++){
             
             let nuovoElemento = createBox("div","box");
             
@@ -75,11 +31,54 @@ if(difficult == "normal"){
         
             
         }
-    }
-}
-
+    } 
+    if(difficult.value == "hard"){
+        container.innerHTML= "";
+        for(let i=1 ; i<=81; i++){
+            
+            let nuovoElemento = createBox("div","box");
+            nuovoElemento.style.width="11%";
+            container.appendChild(nuovoElemento);
     
-} 
+            nuovoElemento.append(i);
+             nuovoElemento.addEventListener("click",
+             function(){
+                nuovoElemento.classList.add("clicked");
+                console.log("Stai cliccando il numero",i);
+             }
+             )
+           
+            
+            
+        
+            
+        }
+    } 
+    if(difficult.value == "very-hard"){
+        container.innerHTML= "";
+        for(let i=1 ; i<=49; i++){
+            
+            let nuovoElemento = createBox("div","box");
+            nuovoElemento.style.width="14%";
+            container.appendChild(nuovoElemento);
+    
+            nuovoElemento.append(i);
+             nuovoElemento.addEventListener("click",
+             function(){
+                nuovoElemento.classList.add("clicked");
+                console.log("Stai cliccando il numero",i);
+             }
+             )
+           
+            
+            
+        
+            
+        }
+    } 
+    }
+
+   
 
 )
 
