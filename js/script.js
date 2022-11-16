97ebe62e248ce6823e52544386d6788a2bcc1750
Yari <narducciyarigmail.com>
2022-11-16 15:23:23 +0100

    creazione lfunzione e ed evento click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 //Campo Minato
+//Creiamo la variabile container dove appenderemo i nostri elementi
+let container = document.getElementById("container-grid");
+
+console.log(container);
+
+let myBtn = document.querySelector("#play");
+
+myBtn.addEventListener("click",
+function(event){
+    let i=1 
+    while( i<=100){
+        let nuovoElemento = createBox();
+        container.appendChild(nuovoElemento);
+        nuovoElemento.append(i);
+        i++
+        
+    
+        
+    }
+}
+)
+
+
 
 //Creiamo la funzione che creerà l'elemento che agganceremo al nostro container
+
+function createBox(){
+    const divElement = document.createElement("div");
+    divElement.classList.add("box");
+   
+
+
+    return divElement;
+}
